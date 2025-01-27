efd924f52bd35c98ddefe5724b73c6484614b5db
AmandineGit <votreemail@example.com>
2025-01-27 23:33:53 +0100

    V2 Presentation et tri des travaux

--- script.js
+++ script.js
@@ -9,14 +9,13 @@
 
 // Récupération des works depuis l'API
 const reponse = await fetch('http://localhost:5678/api/works');
-const listeWorks = await reponse.json();
-console.log(listeWorks);
+const listWorks = await reponse.json();
+console.log(listWorks);
 
 // Fonction de création des fiches de travaux et intégration dans le DOM
-function createWorks(listeWorks) {
-    for (let i = 0; i< listeWorks.length; i++) {
-
-        const figure = listeWorks[i];
+function createWorks(listWorks) {
+    for (let i = 0; i< listWorks.length; i++) {
+        const figure = listWorks[i];
         // Récupération de l'élément du DOM qui accueillera les travaux
         const sectionGallery = document.querySelector(".gallery");
         //creation de la balise qui contiendra les travaux
@@ -35,4 +34,45 @@ function createWorks(listeWorks) {
 }
 
 // Appel de la fonction
-createWorks(listeWorks);
+createWorks(listWorks);
+
+// gestion du bouton de tri "Tous"
+const btnTrier = document.querySelector(".btnTrier");
+
+btnTrier.addEventListener("click", function() {
+    document.querySelector(".gallery").innerHTML = "";
+    createWorks(listWorks);
+});
+
+// gestion du bouton de tri "Objets"
+const btnTrierObjets = document.querySelector(".btnTrierObjets");
+
+btnTrierObjets.addEventListener("click", function() {
+        const listObjets = listWorks.filter(function(figure) {
+            return figure.category.id === 1;
+        });
+        document.querySelector(".gallery").innerHTML = "";
+        createWorks(listObjets);
+});
+
+// gestion du bouton de tri "Appartements"
+const btnTrierAppt = document.querySelector(".btnTrierAppt");
+
+btnTrierAppt.addEventListener("click", function() {
+        const listAppt = listWorks.filter(function(figure) {
+            return figure.category.id === 2;
+        });
+        document.querySelector(".gallery").innerHTML = "";
+        createWorks(listAppt);
+});
+
+// gestion du bouton de tri "Hotels et restaurants"
+const btnTrierHotelsRest = document.querySelector(".btnTrierHotelsRest");
+
+btnTrierHotelsRest.addEventListener("click", function() {
+        const listHoRest = listWorks.filter(function(figure) {
+            return figure.category.id === 3;
+        });
+        document.querySelector(".gallery").innerHTML = "";
+        createWorks(listHoRest);
+});
